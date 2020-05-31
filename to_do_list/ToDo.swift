597bf468b3c3306568ab6e48bf83067253fdd73a
Maria H <alwaysmaria1@gmail.com>
2020-05-31 17:27:03 -0400

    Initial Commit

---
+++ to_do_list/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  to_do_list
+//
+//  Created by Maria H on 5/31/20.
+//  Copyright © 2020 Maria H. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ToDo {
+  var name = ""
+  var important = false
+}
